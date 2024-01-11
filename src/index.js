51085ccf6ad31dc4527f9faec341c1aacd4608b3
JordanDorsey <jordandorsey04@gmail.com>
2024-01-10 21:10:13 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
+    let form = document.querySelector('form')
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      const searchId = event.target.children[1].value
+      // const searchId = document.getElementById('searchByID').value
+  
+      fetch(`http://localhost:3000/movies/${searchId}`) 
+      .then(response => response.json())
+      .then(data => {
+      
+        let title = document.querySelector('section#movieDetails h4')
+        let summary = document.querySelector('section#movieDetails p')
+  
+        title.innerText = data.title
+        summary.innerText = data.summary
+      })
+    })
+    
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
